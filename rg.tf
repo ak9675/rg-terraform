3c09474066b6942079e579cad0f368495f0a0aa8
ak9675 <98090519+ak9675@users.noreply.github.com>
2022-01-20 11:33:05 +0000

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -6,3 +6,7 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "example1" {
+  name     = "example12"
+  location = "eastus"
+}
